3e331f30f595242b5ecf80c155c593bd2c9c5b23
ofthewildfire <103148299+ofthewildfire@users.noreply.github.com>
2024-03-09 18:08:43 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import '@fontsource-variable/overpass';
+import '@fontsource/ubuntu/400.css';
+import '@fontsource/ubuntu/500.css';
+import '@fontsource/ubuntu/700.css';
+import './App.css'
+
+function App() {
+
+    return (
+      <h1 className="text-3xl font-bold underline bg-blue-400">
+        Hello world!
+      </h1>
+    )
+}
+
+export default App
